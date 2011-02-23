bc3fdcea915f8f7b40b551baf4969b5865d68a50
William R. Speirs <bill.speirs@gmail.com>
2011-02-23 11:54:24 -0500

    - Coverted all parameters to lower case on get/set

--- src/com/bittrust/config/BasicModuleConfig.java
+++ src/com/bittrust/config/BasicModuleConfig.java
@@ -23,11 +23,23 @@ public class BasicModuleConfig {
 	public String getClassName() {
 		return className;
 	}
+	
+	/**
+	 * Returns a list of values associated with the parameter name.
+	 * @param param The parameter name, converted to lower case.
+	 * @return A list of values associated with the parameter, or null if not found.
+	 */
 	public List<String> getParams(String param) {
 		return params.get(param);
 	}
 	
+	/**
+	 * Gets a parameter from the configuration.
+	 * @param param The parameter name, converted to lower case.
+	 * @return The value of the parameter, or null if nothing is found.
+	 */
 	public String getParam(String param) {
+		param = param.toLowerCase();
 		List<String> list = params.get(param);
 		
 		if(list != null)
@@ -36,7 +48,13 @@ public class BasicModuleConfig {
 			return null;
 	}
 	
+	/**
+	 * Adds a parameter to the configuration. All param names are converted to lower case first.
+	 * @param param The parameter name, converted to lower case. 
+	 * @param value The value of the parameter.
+	 */
 	public void addParam(String param, String value) {
+		param = param.toLowerCase();
 		
 		if(params.containsKey(param)) {
 			params.get(param).add(value);
