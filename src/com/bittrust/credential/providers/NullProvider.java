0fbb6926a0f3d901007613fbe833d323fdf18050
William R. Speirs <bill.speirs@gmail.com>
2011-02-23 11:53:44 -0500

    - Updated NullProvider to honor existing sessions - Added the ability to take a username from the config file

--- src/com/bittrust/credential/providers/NullProvider.java
+++ src/com/bittrust/credential/providers/NullProvider.java
@@ -7,22 +7,37 @@ import java.util.HashMap;
 
 import com.bittrust.config.BasicModuleConfig;
 import com.bittrust.credential.Credential;
+import com.bittrust.credential.Principal;
+import com.bittrust.http.HttpUtils;
 import com.bittrust.http.PhaaasContext;
 import com.bittrust.session.SessionStore;
 
 /**
  * @class NullProvider
+ * Checks for an existing session or creates a bogus credential.
  */
 public class NullProvider implements CredentialProvider {
 
+	private String username;
+	
 	public NullProvider(BasicModuleConfig config) {
+		this.username = config.getParam("username");
 	}
 	
 	@Override
 	public CredentialProviderResult getCredentialOrPrincipal(SessionStore sessionStore, PhaaasContext context) {
-		context.setCredential(new Credential("", new HashMap<String, String>()));
+		String sessionID = HttpUtils.getCookie(context.getHttpRequest(), SessionStore.SESSION_COOKIE);
+		Principal principal = sessionStore.retrievePrincipal(sessionID);
 		
-		return CredentialProviderResult.CREDENTIAL_FOUND;
+		// create a bogus credential
+		if(principal == null) {
+			context.setCredential(new Credential(username, new HashMap<String, String>()));
+			return CredentialProviderResult.CREDENTIAL_FOUND;
+		} else { // create a bogus principal
+			context.setSessionId(sessionID);	// set the session ID
+			context.setPrincipal(principal);	// store the principal
+			return CredentialProviderResult.PRINCIPAL_FOUND;
+		}
 	}
 
 }
